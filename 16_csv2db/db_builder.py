418e9ef43a9d48243cf3aaf62d47a8dc054e498f
ivzhang1 <izhang1@stuy.edu>
2018-10-04 21:56:20 -0400

    final version

--- 16_csv2db/db_builder.py
+++ 16_csv2db/db_builder.py
@@ -7,28 +7,42 @@ import sqlite3   #enable control of an sqlite database
 import csv       #facilitates CSV I/O
 
 
-name = filename.split(".")[0]
-DB_FILE= "pepes.db"
-
+DB_FILE= "foo.db"
 db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
 c = db.cursor()               #facilitate db ops
-
-#==========================================================
-#INSERT YOUR POPULATE CODE IN THIS ZONE
-command = "CREATE TABLE {table_name}(name TEXT, age INTEGER, id INTEGER)".format(table_name = "pepes")
+command = "CREATE TABLE {table_name}(name TEXT, age INTEGER, id INTEGER PRIMARY KEY)".format(table_name = "peeps")
 c.execute(command)
-
 with open("peeps.csv", newline = "") as csvfile:
     reader = csv.DictReader(csvfile)
     for row in reader:
-        command = 'INSERT INTO pepes VALUES ("{name}", {age}, {id}) '.format(name=row['name'],age=row['age'],id=row['id'])
+        command = 'INSERT INTO peeps VALUES ("{name}", {age}, {id}) '.format(name=row['name'],age=row['age'],id=row['id'])
         c.execute(command)
+db.commit() #save changes
+db.close()  #close database
 
-#command = ""          #build SQL stmt, save as string
-#c.execute(command)    #run SQL statement
+DB_FILE= "foo.db"
+db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
+c = db.cursor()               #facilitate db ops
+command = "CREATE TABLE {table_name}(code TEXT, mark INTEGER, id INTEGER)".format(table_name = "courses")
+c.execute(command)
+with open("courses.csv", newline = "") as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        command = 'INSERT INTO courses VALUES ("{name}", {age}, {id}) '.format(name=row['code'],age=row['mark'],id=row['id'])
+        c.execute(command)
+db.commit() #save changes
+db.close()  #close database
 
-#==========================================================
-
+DB_FILE= "foo.db"
+db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
+c = db.cursor()               #facilitate db ops
+command = "CREATE TABLE {table_name}(job class TEXT, percentage INTEGER)".format(table_name = "occupations")
+c.execute(command)
+with open("occupations.csv", newline = "") as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        command = 'INSERT INTO occupations VALUES ("{job}", {percentage}) '.format(job=row['Job Class'],percentage=row['Percentage'])
+        c.execute(command)
 db.commit() #save changes
 db.close()  #close database
 
