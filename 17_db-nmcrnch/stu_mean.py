9b2ed8e3d3b577479be9b54d3e31975565c79f80
ivzhang1 <izhang1@stuy.edu>
2018-10-08 18:39:35 -0400

    minor changes

--- 17_db-nmcrnch/stu_mean.py
+++ 17_db-nmcrnch/stu_mean.py
@@ -5,18 +5,14 @@
 
 import sqlite3   #enable control of an sqlite database
 import csv       #facilitates CSV I/O
-from functools import reduce
 import db_builder
 #==========================================================
-
 db_builder.main()
 
 DB_FILE="discobandit.db"
-
 db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
 c = db.cursor()               #facilitate db ops
-
-#----------------------
+#======================== our hw ===============================
 
 def name_to_id(name): #Given a student's name, returns their id number, otherwise None
     cmd = 'SELECT id FROM peeps WHERE name = "{}"'.format(name)
@@ -25,9 +21,16 @@ def name_to_id(name): #Given a student's name, returns their id number, otherwis
     if i_d:
         return i_d[0]
     return None
-
+def id_to_name(id):
+    cmd = 'SELECT name FROM peeps WHERE id = "{}"'.format(id)
+    cur = c.execute(cmd)
+    nomen = cur.fetchone()
+    if nomen:
+        return nomen[0]
+    return None
 #print(name_to_id('alison'))
 
+#looks up grades of student
 def getGrades(name): #Returns grades of students in 2D array [[course1, grade1], ...]
     _id = name_to_id(name)
     if not _id:
@@ -42,11 +45,18 @@ def getGrades(name): #Returns grades of students in 2D array [[course1, grade1],
         return l_grades
 
 #print(getGrades('alison'))
+#----------------------^^ specific retrieval functions ^^----------
 
-def gradeBook(): #Gives a gradebook of student grades
+def stuNames():
     cmd = "SELECT name FROM peeps"
     cur = c.execute(cmd)
-    students = cur.fetchall() 
+    return cur.fetchall()# [(name),(name)] 
+#--------------------^^ general retrieval function ^^ ---------------
+
+
+#display each stu's courses and respective grades
+def gradeBook(): #Gives a gradebook of each student's courses and grades
+    students =  stuNames()
     res = ""
     for stu in students:
         list_of_grades = getGrades(stu[0])
@@ -56,10 +66,9 @@ def gradeBook(): #Gives a gradebook of student grades
 
     return res
 print("\n---------------------Gradebook---------------------\n")
-
 print(gradeBook())
 
-def get_average(name):
+def get_average(name):#computes average of a student
     grades = getGrades(name)
     if grades:
         total = 0.0
@@ -69,11 +78,9 @@ def get_average(name):
         return total/len(grades)
     return None
 
-
-def averageBook(): #Gives a gradebook of student grades
-    cmd = "SELECT name FROM peeps"
-    cur = c.execute(cmd)
-    students = cur.fetchall() 
+#display students' averages
+def averageBook(): #Gives average of students
+    students = stuNames()
     res = ""
     for stu in students:
         res += "\nAverage of " + stu[0] + ": " + str(get_average(stu[0]))
@@ -83,10 +90,9 @@ def averageBook(): #Gives a gradebook of student grades
 print("\n---------------------TABLE OF AVERAGES---------------------\n")
 print(averageBook())
 
+#display each student's name, id, and average
 def createGradebook_avg(): #Creates a grade book with name, id, and average
-    cmd = "SELECT name FROM peeps"
-    cur = c.execute(cmd)
-    students = cur.fetchall() 
+    students = stuNames()
     res = ""
     for stu in students:   
         res += "Name: {}, ID: {}, Average = {} \n".format(stu[0], name_to_id(stu[0]), get_average(stu[0]))
@@ -95,24 +101,37 @@ def createGradebook_avg(): #Creates a grade book with name, id, and average
 print("\n---------------------STUDENT DATA---------------------\n")
 print(createGradebook_avg()) 
 
+#----------------------^^ display functions ^^-----------------------
+
 def createAvgTable(): #Creates a table of id, avg
     cmd = "CREATE TABLE peeps_avg (id INTEGER, average REAL)"
     cur = c.execute(cmd)
-    cmd = "SELECT name FROM peeps"
-    cur = c.execute(cmd)
-    students = cur.fetchall() 
+    students = stuNames() 
     res = ""
     for stu in students: 
         cmd = "INSERT INTO peeps_avg VALUES({}, {})".format(name_to_id(stu[0]), get_average(stu[0]))
         cur = c.execute(cmd)
+    return
 createAvgTable()
 db.commit()
 
-def addRowCourses(code, mark, __id):# adds a course, a mark, for a id to courses
-    cmd ='INSERT INTO courses VALUES("{_code}", {_mark}, {_id})'.format(_code = code, _mark = mark, _id = __id)
+#----------------------^^ creates table ^^ -------------------
+
+def changeAvrg(id, new):
+    cmd = "UPDATE peeps_avg SET average = "+str(new)+" WHERE id="+str(id)
     c.execute(cmd)
-    
+    return
+changeAvrg(9, 199)
+#-----------------------^^ cheating fxn ^^--------------------
+
+def addRowCourses(code, mark, id):# adds a course, a mark, for a id to courses
+    cmd ='INSERT INTO courses VALUES("{_code}", {_mark}, {_id})'.format(_code = code, _mark = mark, _id = id)
+    c.execute(cmd)
+    changeAvrg(id,get_average(id_to_name(id)))
+    return
+
 addRowCourses("pokemon", 88, 8)
+#------------------^^ add course function ^^---------------
 
 
 db.commit() #save changes
