2de88de50de63e046fb12f96ef78bfb139533519
ivzhang1 <izhang1@stuy.edu>
2018-11-15 13:19:02 -0500

    cool stuff

---
+++ 26_rrreeesssttt/appy.py
@@ -0,0 +1,52 @@
+#Ivan Zhang
+#SoftDev1 pd7
+#K25: Getting More REST
+#2018-11-14
+
+from flask import Flask, render_template
+import urllib.request
+import json
+import ssl
+import random
+
+
+context = ssl._create_unverified_context()
+
+app = Flask(__name__)
+
+@app.route("/")
+def got():
+	url = "https://api.got.show/api/characters/" # URL
+	cri = urllib.request.Request(url) # SETS UP REQUEST
+	stuff = urllib.request.urlopen(cri, context=context) # GETS STUFF 
+	js = stuff.read() # gets info from urlopen
+	jason = json.loads(js) # loads into JSON
+	#print(jason)
+
+	return render_template("nasa.html", characters = jason)
+
+@app.route("/poke")
+def poke():
+	url = "http://swapi.co/api/planets/{}/".format(str(r)) # URL WITH RANDOM INT
+	cri = urllib.request.Request(url, headers={'User-Agent': 'Mozilla/5.0'}) # SETS UP REQUEST
+	stuff = urllib.request.urlopen(cri, context=context) # GETS STUFF 
+	js = stuff.read() # gets info from urlopen
+	jason = json.loads(js) # loads into JSON
+	#print(jason)
+
+	return render_template("nasa.html", _d = jason)
+
+@app.route("/memey")
+def stars():
+	url = "http://swapi.co/api/planets/{}/".format(str(r)) # URL WITH RANDOM INT
+	cri = urllib.request.Request(url, headers={'User-Agent': 'Mozilla/5.0'}) # SETS UP REQUEST
+	stuff = urllib.request.urlopen(cri, context=context) # GETS STUFF 
+	js = stuff.read() # gets info from urlopen
+	jason = json.loads(js) # loads into JSON
+	#print(jason)
+
+	return render_template("nasa.html", _d = jason)
+
+if __name__ == "__main__":
+	app.debug = True
+	app.run()
