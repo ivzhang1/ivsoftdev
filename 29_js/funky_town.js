07d24b374f4a8acc2a1c4c94fff3b911734085d1
ivzhang1 <izhang1@stuy.edu>
2018-12-19 12:42:50 -0500

    stuff

---
+++ 29_js/funky_town.js
@@ -0,0 +1,39 @@
+//Hi: Ivan Zhang and Jerry Ye
+//SoftDev1 pd7
+//K#29: Sequential Progression
+//2018-12-19
+
+
+var fibby = function (num){
+    if(num == 0){
+	return 0;
+    }
+    if(num < 3){
+	return 1;
+    }
+    return fibby(num - 1) + fibby(num - 2);
+}
+
+var gcd = function (a, b){
+    if(b == 0){
+	return a;
+    }
+    return gcd(b, a % b);
+}
+
+var students = ["Sam", "Tom", "Bobby", "Smith"];
+
+
+var randomStudent = function (){
+    var rand = Math.floor(Math.random() * 4);
+    console.log(students[rand]);
+    return students[rand];
+}
+
+var fib = document.getElementById('fib');
+var gcD = document.getElementById('gcd');
+var randS = document.getElementById('randomStudent');
+
+fib.addEventListener('click', fibby);
+gcD.addEventListener('click', gcd);
+randS.addEventListener('click', randomStudent);
---
+++ 29_js/funky_town.js~
@@ -0,0 +1,24 @@
+var fibby = function (num){
+    if(num == 0){
+	return 0;
+    }
+    if(num < 3){
+	return 1;
+    }
+    return fibby(num - 1) + fibby(num - 2);
+}
+
+var gcd = function (a, b){
+    if(b == 0){
+	return a;
+    }
+    return gcd(b, a % b);
+}
+
+var students = ["Sam", "Tom", "Bobby", "Smith"];
+
+
+var randomStudent = function (){
+    var rand = Math.floor(Math.random() * 4);
+    return students[rand];
+}
